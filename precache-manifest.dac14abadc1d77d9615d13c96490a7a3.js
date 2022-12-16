0916e4caacc69ce2861b421c9a5ab696b8619035
SagarDNG <sagardng123@gmail.com>
2022-12-17 01:38:28 +0530

    Updates

---
+++ precache-manifest.dac14abadc1d77d9615d13c96490a7a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34e057c4e6a3838732e1",
+    "url": "/toDoAppUsingReact/static/css/main.aacedc30.chunk.css"
+  },
+  {
+    "revision": "34e057c4e6a3838732e1",
+    "url": "/toDoAppUsingReact/static/js/main.34e057c4.chunk.js"
+  },
+  {
+    "revision": "204c3ab8380b53cd50a2",
+    "url": "/toDoAppUsingReact/static/js/1.204c3ab8.chunk.js"
+  },
+  {
+    "revision": "abf1b6635834756b556d",
+    "url": "/toDoAppUsingReact/static/js/runtime~main.abf1b663.js"
+  },
+  {
+    "revision": "f94a1d85470af4806d969d4abe1e49cd",
+    "url": "/toDoAppUsingReact/index.html"
+  }
+];
